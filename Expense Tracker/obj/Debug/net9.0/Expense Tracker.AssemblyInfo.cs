53a223c7114f255689344dd498e5550e036538c5
maomen hamdan <110568607+MoamenHamdan@users.noreply.github.com>
2025-06-21 13:27:56 +0300

    final

--- Expense Tracker/obj/Debug/net9.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net9.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa51751c9574e8dd6111a7dcd8380eb14c83021")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160b9cf74fee97a85b12079a5e46bb31127e5c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
