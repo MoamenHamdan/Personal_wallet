f6b1bc9948f139ee371428c756fa2ab1d0499817
maomen hamdan <110568607+MoamenHamdan@users.noreply.github.com>
2025-06-27 19:44:33 +0300

    Update assembly information and project directory paths; add new image file for uploads.

--- Expense Tracker/obj/Debug/net9.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net9.0/Expense Tracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+512b99dcf17d7fba378cfb222dc097e375cd2c6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a2d78ce19f72c1c6b97e09b71d1310a53bcff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
